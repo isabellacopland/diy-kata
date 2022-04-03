1fe02e7522078131c335ff581da53b4f59457a18
isabellacopland <isabella.copland@gmail.com>
2022-04-03 16:04:17 +0100

    updated fizzbuzz with tests

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,7 +1,7 @@
 const fizzBuzz = number => {
-    if (number % 3 === 0) {return "Fizz"};
-    if (number % 5 === 0) {return "Buzz"};
-    if (number % 15 === 0) {return "FizzBuzz"}
+    if (number % 5 === 0 && number % 3 === 0) {return 'FizzBuzz'};
+    if (number % 3 === 0) {return 'Fizz'};
+    if (number % 5 === 0) {return 'Buzz'}
     else {return number};
 };
 
