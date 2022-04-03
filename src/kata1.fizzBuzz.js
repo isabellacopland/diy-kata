a1f01fdd09123bb4a17ae3e8d50037e6987d14a9
isabellacopland <isabella.copland@gmail.com>
2022-04-03 15:35:26 +0100

    fizzbuzz completed

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,8 @@
-const fizzBuzz = number => {};
+const fizzBuzz = number => {
+    if (number % 3 === 0) {return "Fizz"};
+    if (number % 5 === 0) {return "Buzz"};
+    if (number % 15 === 0) {return "FizzBuzz"}
+    else {return number};
+};
 
 module.exports = fizzBuzz;
