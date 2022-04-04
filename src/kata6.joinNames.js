d9d6706d8c068ae57cb3849486145616d55e2b07
isabellacopland <isabella.copland@gmail.com>
2022-04-04 20:50:27 +0100

    join names added

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,19 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+    
+    class Person {
+        constructor(name) {
+            this.name = name
+        }
+    }
+
+    const bart = new Person ('Bart')
+    const lisa = new Person ('Lisa')
+    const maggie = new Person ('Maggie')
+    const tony = new Person ('Tony')
+
+const finalName = namesObj.pop();
+
+return namesObj.join(', ') + ' & ' + finalName;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,8 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  it("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames(['Bart', 'Lisa', 'Maggie'])).toEqual('Bart, Lisa & Maggie')
+    expect(joinNames(['Bart', 'Lisa', 'Maggie', 'Tony'])).toEqual('Bart, Lisa, Maggie & Tony')
+  });
 });
