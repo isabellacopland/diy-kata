412d57668435447bab0499f92efa895b1bfbd60c
isabellacopland <isabella.copland@gmail.com>
2022-04-04 19:34:16 +0100

    reachDestination added

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,6 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    const hours = (distance / speed);
+   return "I should be there in " + (Math.round(hours*2)/2) + " hours"
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,9 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  it("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toEqual('I should be there in 4.5 hours')
+    expect(reachDestination(50, 2)).toEqual('I should be there in 25 hours')
+    expect(reachDestination(20, 10)).toEqual('I should be there in 2 hours')
+  });
 });
