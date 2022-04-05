dcd95c760100a5a9298afa0a3c24482889be183b
isabellacopland <isabella.copland@gmail.com>
2022-04-05 21:03:36 +0100

    getEmployerRole added

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,17 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+
+    if (!employees) {
+        return "Not an employee";
+    }
+
+    const foundEmployee = employees.find(
+        element => element.name === employeeName);
+    
+    if (!foundEmployee) {
+        return "Does not work here";
+    }
+
+    return foundEmployee.role;
+};
 
 module.exports = getEmployerRole;
